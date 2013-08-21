0d4982aeb0d62ae14dc3327c50b0daa9cd0dd3c2
mbecker73 <msbecker@udel.edu>
2013-08-20 23:55:32 -0400

    Solution to Problem 10

--- src/SummationOfPrimes.java
+++ src/SummationOfPrimes.java
@@ -5,27 +5,38 @@
  */
 
 public class SummationOfPrimes {
+	
     public static void main(String[] args) {
-	System.out.println(sumOfPrimes(2000000));
+    	System.out.println(sumOfPrimes(2000000));
     }
-    public static int sumOfPrimes(int limit){
-	int sum=0;
-	int n=1;
+    
+    public static long sumOfPrimes(long limit){
+    	long sum=0;
+	
+    	//create boolean array holding if number at index is prime or not
+    	boolean[] primeArray = new boolean[(int) (limit + 1)];
+    	for (int i = 2; i <= limit; i++) {
+    		primeArray[i] = true; //init all as true
+    	}
+    	
+    	for (int i = 2; i*i <= limit; i++) {
 
-	while(n < limit){
-		n++;
-		if(isPrime(n))
-			sum += n;
-	}
-	return sum;
-    }
-
-    //boolean function to check if a number is prime
-    public static boolean isPrime(int n) {
-        for(int i =2; i < n; i++) {
-            if(n % i == 0) //has a divisor besides 1 and itself
-                return false;
+            // if i is prime, then mark multiples of i as nonprime
+            // suffices to consider mutiples i, i+1, ..., N/i
+            if (primeArray[i]) {
+                for (int j = i; i*j <= limit; j++) {
+                    primeArray[i*j] = false;
+                }
+            }
         }
-        return true;
+
+    	//loop through all numbers 2 through 2 million
+    	for(int x=2; x < limit; x++){
+    		if(primeArray[x]) //if that number is prime then increment sum 
+    			sum += x;
+    	}
+
+    	return sum;
     }
+
 }
