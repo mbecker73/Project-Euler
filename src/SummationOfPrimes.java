08bf7efd19a3c48d92f8057d380cc53b8132a8dd
mbecker73 <msbecker@udel.edu>
2013-08-24 20:18:32 -0400

    Further optimized solution to Problem #10

--- src/SummationOfPrimes.java
+++ src/SummationOfPrimes.java
@@ -11,28 +11,27 @@ public class SummationOfPrimes {
     }
     
     public static long sumOfPrimes(long limit){
-    	long sum=0;
-
+    	//calculate summation of all numbers less than the limit and greater than 1
+    	long sum =  ((limit)*(limit+1) / 2) -limit -1;	
+    	    	
     	//create boolean array holding if number at index is prime or not
-    	boolean[] primeArray = new boolean[(int) (limit + 1)];
-    	for (int i = 2; i <= limit; i++) {
+    	boolean[] primeArray = new boolean[(int) (limit)];
+    	for (int i = 2; i < limit; i++) {
     		primeArray[i] = true; //init all as true
     	}
+    	
     	//Sieve of Eratosthenes implementation
-    	for (int i = 2; i*i <= limit; i++) {
+    	for (int i = 2; i*i < limit; i++) {
             if (primeArray[i]) { //if the number is marked as a possible prime
             	//mark every "i"th number as false (all multiples of i)
-                for (int j = i; i*j <= limit; j++) {
-                    primeArray[i*j] = false;
+                for (int j = i; i*j < limit; j++) {
+                	if(primeArray[i*j] ){ //to prevent duplicating subtraction from total
+                		primeArray[i*j] = false;
+                		sum -= i*j; //subtract value that is not prime from total sum
+                	}
                 }
             }
         }
-
-    	//loop through all numbers 2 through 2 million
-    	for(int x=2; x < limit; x++){
-    		if(primeArray[x]) //if that number is prime then increment sum 
-    			sum += x;
-    	}
 
     	return sum;
     }
