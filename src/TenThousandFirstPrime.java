a598d41c493c4e47d9510cf419c29ec7a3396e02
mbecker73 <msbecker@udel.edu>
2013-09-26 01:13:00 -0400

    Solution to Problem 7 complete

--- src/TenThousandFirstPrime.java
+++ src/TenThousandFirstPrime.java
@@ -4,7 +4,7 @@
  * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
  * the 6th prime is 13.
  * 
- * What is the 10 001st prime number?
+ * What is the 10,001st prime number?
  */
 
 public class TenThousandFirstPrime {
@@ -12,22 +12,39 @@ public class TenThousandFirstPrime {
 	public static void main(String[] args) throws Exception {
 		double start = System.currentTimeMillis();
 		
-		int primeCount =3;
-		int num =3;
+		int primeCount =1;
+		int testNumber =1;
+		
 		while(primeCount < 10001){
-			num++;
-			if(isPrime(num)){
+			testNumber += 2; //only need to try odd numbers
+			if(isPrime(testNumber)) {
 				primeCount++;
 			}
 		}
 		
+		System.out.println(testNumber);
+		
 		System.out.println(System.currentTimeMillis() -start +"ms");
 	}
 	
+	/*
+	 * Method to return boolean indicating if an integer is a prime number or not
+	 */
 	public static boolean isPrime(int n){
-		boolean isPrime =false;
-	
-		return isPrime;
+
+		//all even numbers and numbers less than 2 are not prime
+		if(n % 2 == 0 || n < 2){
+			return false;
+		}
+		for (int i=3; i*i<=n; i = i + 2) {
+			//check if number i divides evenly into n
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		
+		//otherwise it is a prime number
+		return true;
 		
 	}
 }
